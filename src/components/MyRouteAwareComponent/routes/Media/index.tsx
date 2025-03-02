60db1d5bba9d51fc3b4b63f2dc47469eeb21938a
dk <hi@mrkha.ng>
2025-03-02 04:26:30 +0000

    exp 0.16.0-alpha.1

--- src/components/MyRouteAwareComponent/routes/Media/index.tsx
+++ src/components/MyRouteAwareComponent/routes/Media/index.tsx
@@ -2,13 +2,14 @@ import type { FunctionComponent } from "preact";
 import type { RoutableProps } from "preact-iso";
 
 type Props = RoutableProps & {
-  id?: string
-}
+  id?: string;
+};
 
 export const Media: FunctionComponent<Props> = ({ id }) => {
-
-  return <section className="page-media">
-    {id && <h1>Showing media for id {id}</h1>}
-    {!id && <small>Please provide a source url to view its content</small>}
-  </section>;
-}
+  return (
+    <section className="page-media">
+      {id && <h1>Showing media for id {id}</h1>}
+      {!id && <small>Please provide a source url to view its content</small>}
+    </section>
+  );
+};
